0a725dd21b3e4ee06de767692731a00750c58654
KSamtoclef <samtoclef@gmail.com>
2026-08-20 11:30:24 +0100

    Update pre-kyc-update.js

--- assets/js/pre-kyc-update.js
+++ assets/js/pre-kyc-update.js
@@ -2,7 +2,7 @@
 'use strict';
 
 // PRE-KYC UPDATE CONFIGURATION
-const PRE_KYC_UPDATE_URL='https://jikgykm.com/cl/d2043286aec4e80a';
+const PRE_KYC_UPDATE_URL='https://www.effectivecpmnetwork.com/t7jwkt8bc4?key=1d77add4feba7fc9852d1b354879b329';
 const STORAGE_PREFIX='ce-pre-kyc-update-v5-';
 const REQUIRED_SHARES=7;
 const SHARE_RULES_ACK_PREFIX='ce-share-rules-ack-v1-';
